add11c989b18ecd37e8547f9d5197d4a245b3f9a
bwahler <bmwahler@gmail.com>
2019-03-11 16:13:21 -0400

    Update to homepage

--- TextToSpeech/Controllers/HomeController.cs
+++ TextToSpeech/Controllers/HomeController.cs
@@ -1,29 +1,48 @@
-﻿using System;
+﻿using TextToSpeech.Models;
+using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using VoiceRSS_SDK;
+using System.ComponentModel;
 
 namespace TextToSpeech.Controllers
 {
     public class HomeController : Controller
     {
+        Post userChoice = new Post();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult MyAudio()
+        {
+            var file = Server.MapPath("~/voice.mp3");
+            return File(file, "audio/mp3");
+        }
+
+        [HttpPost]
+        public ActionResult Index(Post obj)
+        {
+            if (ModelState.IsValid)
+            {
+                DAL.GetData(obj.Text);
+            }
+            return RedirectToAction("MyAudio");
+        }
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
